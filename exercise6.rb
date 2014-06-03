15bf904451cc2f5fca1567e380ffad96626cb630
davidhurley <dhurls99@gmail.com>
2014-06-03 16:15:34 -0400

    answer to exercise 6

---
+++ exercise6.rb
@@ -0,0 +1,27 @@
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+grocery_list << "rice"
+
+def print_out_list(arr)
+	arr.sort_by!{|word| word.downcase}
+	arr.each do |item|
+		puts "* " + item
+	end
+	puts "Total items in list is #{arr.count}"
+	if arr.include?("bananas")
+		puts "You need to pick up bananas"
+	else
+		puts "You don't need to pick up bananas today"
+	end
+	puts "The 2nd item in the list is #{arr[1]}"
+end
+
+print_out_list(grocery_list)
+
+def delete_salmon(arr)
+	arr.delete_at(arr.index('salmon')) if arr.include?("salmon")
+	puts "the new list: "
+	print_out_list(arr)
+end
+
+puts delete_salmon(grocery_list)
