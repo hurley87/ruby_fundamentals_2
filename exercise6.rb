ed578e320da6b44a07e510d241da3bfca21868d8
davidhurley <dhurls99@gmail.com>
2014-06-04 15:39:26 -0400

    make final changes to exercises

--- exercise6.rb
+++ exercise6.rb
@@ -8,10 +8,11 @@ def print_out_list(arr)
 		puts "* " + item
 	end
 	puts "Total items in list is #{arr.count}"
+
 	if arr.include?("bananas")
-		puts "You need to pick up bananas"
-	else
 		puts "You don't need to pick up bananas today"
+	else
+		puts "You need to pick up bananas"
 	end
 	puts "The 2nd item in the list is #{arr[1]}"
 end
